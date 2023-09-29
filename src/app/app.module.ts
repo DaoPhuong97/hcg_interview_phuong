d16a80e8999bd4b94372629b0e9564ba387cd72e
Phuong Dao <daothiphuong1997@gmail.com>
2023-09-29 23:14:22 +0700

    setup project

---
+++ src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { AdvancedSearchComponent } from './pages/advanced-search/advanced-search.component';
+import { NavbarComponent } from './common-components/navbar/navbar.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from './material.module';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomePageComponent,
+    AdvancedSearchComponent,
+    NavbarComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MaterialModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
